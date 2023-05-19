648fefc0acae14a9fff93c3c98335e018b86774f
rikkkk1206 <csri18065@g.nihon-u.ac.jp>
2023-05-19 15:30:48 +0900

    プログラム構造をリファクタリング #5
    インデントがずれている箇所があったため修正。

--- iOSEngineerCodeCheck/SearchRepository/SearchRepositoryModel.swift
+++ iOSEngineerCodeCheck/SearchRepository/SearchRepositoryModel.swift
@@ -19,34 +19,32 @@ final class SearchRepositoryModel: SearchRepositoryModelInput {
     
     var searchingSessionTask: URLSessionTask? = nil
     
-    func fetchRepository(
-        inputText: String,
-        completion: @escaping ([Repository]) -> ()) {
-            guard let encodedInputText = inputText.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) else {
-                print("failed encode \(inputText) for URL")
-                return
-            }
-            let urlString = "https://api.github.com/search/repositories?q=\(encodedInputText)"
-            guard let url = URL(string: urlString) else {
-                print("urlString(\(urlString)) is invalid")
-                return
-            }
-            searchingSessionTask = URLSession.shared.dataTask(with: url) { (data, res, err) in
-                if let data = data {
-                    do {
-                        let response = try JSONDecoder().decode(SearchRepositoryResponse.self, from: data)
-                        completion(response.items)
-                    } catch {
-                        print(error)
-                    }
-                }
-                if let err = err {
-                    print(err)
-                }
-            }
-            // APIへの問い合わせを実行
-            searchingSessionTask?.resume()
+    func fetchRepository(inputText: String, completion: @escaping ([Repository]) -> ()) {
+        guard let encodedInputText = inputText.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) else {
+            print("failed encode \(inputText) for URL")
+            return
         }
+        let urlString = "https://api.github.com/search/repositories?q=\(encodedInputText)"
+        guard let url = URL(string: urlString) else {
+            print("urlString(\(urlString)) is invalid")
+            return
+        }
+        searchingSessionTask = URLSession.shared.dataTask(with: url) { (data, res, err) in
+            if let data = data {
+                do {
+                    let response = try JSONDecoder().decode(SearchRepositoryResponse.self, from: data)
+                    completion(response.items)
+                } catch {
+                    print(error)
+                }
+            }
+            if let err = err {
+                print(err)
+            }
+        }
+        // APIへの問い合わせを実行
+        searchingSessionTask?.resume()
+    }
     
     func searchingSessionCancel() {
         searchingSessionTask?.cancel()
